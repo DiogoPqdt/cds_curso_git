b0d3058dfbe5a91b7b7cc850fccf0921b56b8567
Diogo Expedito <aspdiogo346@gmail.com>
2023-10-07 07:55:01 -0300

    Adicionado função principal

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import streamlit as st
 def load_data():
     return pd.read_csv(r'D:\OneDrive\Pessoal\2. Data Science\Comunidade_DS\repos\repos\Git\projeto\data\processed\bikes_completed.csv')
 
-
-df = load_data()
-
-st.dataframe(df)
+def main():
+    df = load_data()
+    st.dataframe(df)
+    
+if __name__ == '__main__':
+    main()
